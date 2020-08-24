7024ed9027247d26031e48684daf92629ce59596
Peter Meissner <petermeissner@users.noreply.github.com>
2020-08-24 07:29:10 +0200

    Expand pings()' timeout parameter description

--- pythonping/__init__.py
+++ pythonping/__init__.py
@@ -24,7 +24,7 @@ def ping(target,
 
     :param target: The remote hostname or IP address to ping
     :type target: str
-    :param timeout: How long before considering each non-arrived reply permanently lost
+    :param timeout: Time in seconds before considering each non-arrived reply permanently lost.
     :type timeout: int, float
     :param count: How many times to attempt the ping
     :type count: int
